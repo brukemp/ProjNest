def3ce022b02bc3807177187fabf4eb6b5885cec
Bruna Kempinsk <kempinskb@gmail.com>
2024-06-13 18:07:16 -0300

    Novas Atualizações

---
+++ src/users/domain/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import { Entity } from "@/shared/domain/entities/entity"
+import { UserValidatorFactory } from "../validators/user.validator"
+
+export type UserProps = {
+    name: String
+    email: String
+    password: String
+    createdAt?: Date
+}
+
+export class UserEntity extends Entity<UserProps>{
+    constructor(public readonly props: UserProps, id?:string){
+        UserEntity.validate(props)
+        super(props, id)
+        this.props.createdAt = this.props.createdAt ?? new Date()
+    }
+
+    get name(): String {
+        return this.props.name
+    }
+
+    private set name(value: string){
+        this.props.name = value
+    }
+
+    get email(): String {
+        return this.props.email
+    }
+
+    get password(): String {
+        return this.props.password
+    }
+
+    private set password(value: string){
+        this.props.password = value
+    }
+
+    get CreateAt(): Date {
+        return this.props.createdAt
+    }
+
+    update(value: string):void{
+        UserEntity.validate({...this.props, name: value})
+        this.name = value
+    }
+    updatePassword(value: string):void{
+        UserEntity.validate({...this.props, password: value})
+        this.password = value
+    }
+    static validate(props: UserProps) {
+        const validator = UserValidatorFactory.create()
+        validator.validate(props)
+    }
+}
